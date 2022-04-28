59d4cd122a130adb9d3364b22cf4c555c9f0f5b5
Shriya <shriya.atmak@gmail.com>
2022-04-28 14:40:52 -0400

    File upload and download working in UI

--- ui/src/script.js
+++ ui/src/script.js
@@ -4,23 +4,23 @@ const decryptionUrl = "http://localhost:8080/decrypt";
 function fileType(){
   var fileType = document.getElementById("fileType");
   fileType.addEventListener('change', changeAcceptType, false);
-
+  
   function changeAcceptType(e) {
     fileTypeStr = e.target.value;
     acceptType = "";
     switch(fileTypeStr){
       case "TEXT":
-        acceptType = ".doc,.pdf,.txt";
-        break;
+      acceptType = ".doc,.pdf,.txt";
+      break;
       case "IMAGE":
-        acceptType = "image/*";
-        break;
+      acceptType = "image/*";
+      break;
       case "VIDEO":
-        acceptType = "video/*";
-        break;
+      acceptType = "video/*";
+      break;
       case "AUDIO":
-        acceptType = "audio/*";
-        break;
+      acceptType = "audio/*";
+      break;
     }
     fileUploader = document.getElementById('file-upload');
     fileUploader.setAttribute("accept", acceptType);
@@ -32,15 +32,15 @@ function fileType(){
 function ekUpload(){
   var files;
   function Init() {
-
+    
     console.log("Upload Initialised");
-
+    
     var fileSelect    = document.getElementById('file-upload'),
-        fileDrag      = document.getElementById('file-drag'),
-        submitButton  = document.getElementById('submit-button');
-
+    fileDrag      = document.getElementById('file-drag'),
+    submitButton  = document.getElementById('submit-button');
+    
     fileSelect.addEventListener('change', fileSelectHandler, false);
-
+    
     // Is XHR2 available?
     var xhr = new XMLHttpRequest();
     if (xhr.upload) {
@@ -49,107 +49,122 @@ function ekUpload(){
       fileDrag.addEventListener('dragleave', fileDragHover, false);
       fileDrag.addEventListener('drop', fileSelectHandler, false);
     }
-
+    
     var startButton = document.getElementById('startButton')
-    startButton.addEventListener('click', uploadFiles);
+    startButton.addEventListener('click', uploadFile);
   }
-
+  
   function fileDragHover(e) {
     var fileDrag = document.getElementById('file-drag');
-
+    
     e.stopPropagation();
     e.preventDefault();
-
+    
     fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
   }
-
+  
   function fileSelectHandler(e) {
     // Fetch FileList object
     files = e.target.files || e.dataTransfer.files;
-
+    
     // Cancel event and hover styling
     fileDragHover(e);
-
+    
     // Process all File objects
     for (var i = 0, f; f = files[i]; i++) {
       parseFile(f);
     }
   }
-
+  
   // Output
   function output(msg) {
     // Response
     var m = document.getElementById('messages');
     m.innerHTML = msg;
   }
-
+  
   function parseFile(file) {
-
+    
     console.log(file.name);
     output(
       '<strong>' + encodeURI(file.name) + '</strong>'
-    );
+      );
+      
+      // var fileType = file.type;
+      // console.log(fileType);
+      var fileName = file.name;
+      
+      document.getElementById('start').classList.add("hidden");
+      document.getElementById('response').classList.remove("hidden");
+    }
     
-    // var fileType = file.type;
-    // console.log(fileType);
-    var fileName = file.name;
-
-    document.getElementById('start').classList.add("hidden");
-    document.getElementById('response').classList.remove("hidden");
-  }
-
-  function setProgressMaxValue(e) {
-    var pBar = document.getElementById('file-progress');
-
-    if (e.lengthComputable) {
-      pBar.max = e.total;
+    function setProgressMaxValue(e) {
+      var pBar = document.getElementById('file-progress');
+      
+      if (e.lengthComputable) {
+        pBar.max = e.total;
+      }
     }
-  }
-
-  function updateFileProgress(e) {
-    var pBar = document.getElementById('file-progress');
-
-    if (e.lengthComputable) {
-      pBar.value = e.loaded;
+    
+    function updateFileProgress(e) {
+      var pBar = document.getElementById('file-progress');
+      
+      if (e.lengthComputable) {
+        pBar.value = e.loaded;
+      }
     }
-  }
-
-  function uploadFiles() {
-    for (var i = 0, f; f = files[i]; i++) {
-      uploadFile(f);
-    }
-  }
-
-  function uploadFile(file) {
-
-    var xhr = new XMLHttpRequest(),
-      pBar = document.getElementById('file-progress'),
-      fileSizeLimit = 1024; // In MB
-    if (xhr.upload) {
-      // Check if file is less than x MB
-      if (file.size <= fileSizeLimit * 1024 * 1024) {
+    
+    function uploadFile() {
+      
+      var xhr = new XMLHttpRequest(),
+      pBar = document.getElementById('file-progress')
+      
+      if (xhr.upload) {
+        // Check if file is less than x MB
         // Progress bar
         pBar.style.display = 'inline';
         xhr.upload.addEventListener('loadstart', setProgressMaxValue, false);
         xhr.upload.addEventListener('progress', updateFileProgress, false);
-
+        
         // File received / failed
         xhr.onreadystatechange = function(e) {
           if (xhr.readyState == 4) {
-            downloadFile(xhr.response)
+            var headers = xhr.getAllResponseHeaders();
+            
+            // Convert the header string into an array
+            // of individual headers
+            var arr = headers.trim().split(/[\r\n]+/);
+            
+            // Create a map of header names to values
+            var headerMap = {};
+            arr.forEach(function (line) {
+              var parts = line.split(': ');
+              var header = parts.shift();
+              var value = parts.join(': ');
+              headerMap[header] = value;
+            });
+            downloadFile(xhr.response, headerMap)
           }
         };
         pwd = document.getElementById("pwd").value;
         var radios = document.getElementsByName("selectMode");
         var selected = Array.from(radios).find(radio => radio.checked);
+        if (selected == null){
+          alert("Mode must be selected.")
+        }
+        if (pwd == null){
+          alert("Password required.")
+        }
         var mode = selected.value;
         var fileTypeSelected = document.getElementById('fileType').value
-
+        
         if (mode == "Encrypt"){
-          reqUrl = encryptionUrl; 
+          reqUrl = encryptionUrl;
+          requestName = "encryptionRequest"; 
         }
         else {
           reqUrl = decryptionUrl;
+          requestName = "decryptionRequest";
         }
         var req = {
           fileType: fileTypeSelected,
@@ -157,38 +172,70 @@ function ekUpload(){
           encryptionLevel: 'STRONG'
         }
         var formData = new FormData();
-        formData.append("file", file)
-        formData.append("encryptionRequest", new Blob([JSON.stringify(req)], {type: "application/json"}))
+        for (var i = 0, f; f = files[i]; i++) {
+          formData.append("file", files[i])
+        }
+        formData.append(requestName, new Blob([JSON.stringify(req)], {type: "application/json"}))
         // Start upload
         xhr.open('POST', reqUrl, true);
-        //xhr.setRequestHeader()
-        xhr.responseType = 'text';
+        xhr.responseType = "blob";
         xhr.send(formData);
-      } else {
-        output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
+
+        //$("#myModal").modal()
       }
+    }
+    
+    function downloadFile(response, headerMap) {
+      //$("#myModal").modal({ show: false})
+      var blobUrl = URL.createObjectURL(response);
+      var link = document.createElement("a"); // Or maybe get it from the current document
+      link.href = blobUrl;
+      link.download = "encrypted_file";
+      link.innerHTML = "Click here to download the file";
+      document.body.appendChild(link); // Or append it whereever you want
+      document.querySelector('a').click()
+    }
+
+    function base64ToArrayBuffer(base64) {
+      var binaryString = window.atob(base64);
+      var binaryLen = binaryString.length;
+      var bytes = new Uint8Array(binaryLen);
+      for (var i = 0; i < binaryLen; i++) {
+         var ascii = binaryString.charCodeAt(i);
+         bytes[i] = ascii;
+      }
+      return bytes;
+   }
+   function saveByteArray(reportName, byte) {
+    var blob = new Blob([byte], {type: "application/octet-stream"});
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    var fileName = reportName;
+    link.download = fileName;
+    link.click();
+  };
+
+    
+    // Check for the various File API support.
+    if (window.File && window.FileList && window.FileReader) {
+      Init();
+    } else {
+      document.getElementById('file-drag').style.display = 'none';
     }
   }
 
-  function downloadFile(response) {
-    var contentDispo = response.getResponseHeader('Content-Disposition');
-    var fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
-    saveBlob(response, fileName);
+  function passwordToggle(){
+    const togglePassword = document.getElementById('togglePassword');
+    const password = document.getElementById('pwd');
+    togglePassword.addEventListener('click', function (e) {
+      // toggle the type attribute
+      const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+      // toggle the eye / eye slash icon
+      this.classList.toggle('bi-eye');
+    });
   }
 
-  function saveBlob(blob, fileName) {
-    var a = document.createElement('a');
-    a.href = window.URL.createObjectURL(blob);
-    a.download = fileName;
-    a.dispatchEvent(new MouseEvent('click'));
-  }
-
-  // Check for the various File API support.
-  if (window.File && window.FileList && window.FileReader) {
-    Init();
-  } else {
-    document.getElementById('file-drag').style.display = 'none';
-  }
-}
-ekUpload();
-fileType();
+  ekUpload();
+  fileType();
+  passwordToggle();
