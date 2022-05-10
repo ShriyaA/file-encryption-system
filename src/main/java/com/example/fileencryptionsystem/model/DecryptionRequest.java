fc1786729a573c52282a1e1cd8c9bb8ee3fc9615
pranavkapoor0808 <pranavparaskapoor@gmail.com>
2022-05-09 20:13:58 -0400

    fix lombok issues

--- src/main/java/com/example/fileencryptionsystem/model/DecryptionRequest.java
+++ src/main/java/com/example/fileencryptionsystem/model/DecryptionRequest.java
@@ -1,11 +1,31 @@
 package com.example.fileencryptionsystem.model;
 
-import lombok.Data;
-
-@Data
 public class DecryptionRequest {
-
   String key;
   FileType fileType;
   EncryptionLevel encryptionLevel;
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public FileType getFileType() {
+    return fileType;
+  }
+
+  public void setFileType(FileType fileType) {
+    this.fileType = fileType;
+  }
+
+  public EncryptionLevel getEncryptionLevel() {
+    return encryptionLevel;
+  }
+
+  public void setEncryptionLevel(EncryptionLevel encryptionLevel) {
+    this.encryptionLevel = encryptionLevel;
+  }
 }
